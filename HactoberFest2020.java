acb6d9c29816f64e29ccce845c75c8070ace77c9
Raghav-Bajaj <73180455+Raghav-Bajaj@users.noreply.github.com>
2020-10-21 21:03:19 +0530

    Create HactoberFest2020.java

---
+++ HactoberFest2020.java
@@ -0,0 +1,13 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class HactoberFest2020
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	System.out.println("Hello everyone");
+	System.out.println("Happy HactoberFest2020");
+	}
+}
